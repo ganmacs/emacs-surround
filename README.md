1326430ffcbadc1e3f3c1428b9054e4da30856d5
ganmacs <ganmacs@gmail.com>
2015-01-25 10:31:00 +0900

    ver 0.0.1

--- README.md
+++ README.md
@@ -2,4 +2,59 @@
 
 Emacs version of the vim.surround.
 
+## Installation
 
+Use `cask` or `el-get` or clone form `https://github.com/ganmacs/emacs-surround.git`
+
+```
+(require 'emacs-surround)
+(global-set-key (kbd "C-q") 'emacs-surround)
+```
+
+## Example
+
+### change
+
+Press `C-t "'`
+
+```
+"Hello| world!"
+```
+
+change it to
+
+```
+'Hello world!'
+```
+
+### insert
+
+Press `C-t i"` or `C-t " <return>`
+
+```
+Hel|lo
+```
+
+change it to
+
+```
+"Hello"
+```
+
+### delete
+
+Press `C-t d"`
+
+```
+"Hello \"world\""
+```
+
+change it to
+
+```
+Hel|lo \"world\"
+```
+
+## TODO
+
+* 場所を覚えておく
--- emacs-surround.el
+++ emacs-surround.el
@@ -1,6 +1,6 @@
-;;; emacs-git-grep.el --- interface of git grep with helm
+;;; emacs-surround.el --- surround symbol or change surrouning char -*- lexical-binding: t -*-
 
-;; Copyright (C) 2013-2014 ganmacs
+;; Copyright (C) 2015 ganmacs
 
 ;; Author: ganmacs <ganmacs_at_gmail.com>
 ;; Maintainer: ganmacs <ganmacs_at_gmail.com>
@@ -26,5 +26,156 @@
 
 ;;; Code:
 
+(defgroup emacs-surround nil
+  "surround.vim for Emacs"
+  :group 'surround)
+
+(defcustom emacs-surround-alist
+  '((""   . (""  . ""))
+    ("'"  . ("'" . "'"))
+    ("("  . ("(" . ")"))
+    (")"  . ("(" . ")"))
+    ("{"  . ("{" . "}"))
+    ("}"  . ("{" . "}"))
+    ("["  . ("[" . "]"))
+    ("]"  . ("[" . "]"))
+    ("/"  . ("/" . "/"))
+    ("\"" . ("\"" . "\"")))
+  "Surround key list."
+  :group 'surround)
+
+(defvar emacs-surround-separator "^\s()[]:;,=.\n{}")
+
+(defun emacs-surround-mark-region-line ()
+  "Mark region at line."
+  (back-to-indentation)
+  (set-mark (point))
+  (move-end-of-line 1))
+
+(defun emacs-surround-mark-region-sep ()
+  "Mark region with emacs-surround-separator."
+  (skip-chars-backward emacs-surround-separator)
+  (set-mark (point))
+  (skip-chars-forward emacs-surround-separator))
+
+(defun emacs-surround-same-count-p (str a b)
+  "STR Count A, B appear number in STR."
+  (cl-flet ((count-match-str (regex)
+                             (with-temp-buffer
+                               (insert str)
+                               (goto-char (point-min))
+                               (count-matches regex))))
+    (not (= (count-match-str a)
+            (count-match-str b)))))
+
+(defun emacs-surround-quote-p (type)
+  "Return quote string or not.
+check TYPE."
+  (let ((min (point-at-bol))
+        (max (point-at-eol))
+        (ppoint (if (eq type 'forward) (point) (- (point) 1))))
+    (defun iter (i p)
+      (if (<= min p max)
+          (if (= (char-before p) 92) ; quote
+              (iter (+ i 1) (- p 1))
+            (= (mod i 2) 1))))
+    (iter 0 ppoint)))
+
+(defun emacs-surround-mark-between (prefix &optional suffix)
+  "Wrap str with PREFIX and SUFFIX."
+  (cl-flet ((search-prefix () (search-backward prefix (point-at-bol) nil 1))
+            (search-suffix () (search-forward (or suffix prefix) (point-at-eol) nil 1)))
+    (if (search-prefix)
+        (progn
+          (while (emacs-surround-quote-p 'forward) (search-prefix))
+          (set-mark (point))
+          (forward-char)
+          (search-suffix)
+          (while (if (string= prefix suffix)
+                     (emacs-surround-quote-p 'backward)
+                   (emacs-surround-same-count-p
+                    (buffer-substring (region-beginning) (region-end))
+                    prefix
+                    suffix))
+            (search-suffix))))))
+
+(defun emacs-surround-get-alist (key)
+  "Get list by emacs-surround-alit with KEY."
+  (cdr (or (assoc key emacs-surround-alist)
+           (assoc "" emacs-surround-alist))))
+
+(defun emacs-surround-wrap (str prefix &optional suffix)
+  "Wrap STR with PREFIX and SUFFIX(if suffix exists)."
+  (concat prefix str (or suffix prefix)))
+
+(defun emacs-surround-replace (str from to)
+  "STR FROM TO.
+(FROM)STR(FROM) -> (TO)STR(TO).
+Switch FROM surrounding STR to TO"
+  (let* ((f-prefix (car from)) (f-suffix (cdr from))
+         (t-prefix (car to)) (t-suffix (cdr to))
+         (regx (format "^%s\\(.*\\)%s$" f-prefix f-suffix)))
+    (if (string-match regx str)
+        (let ((match (match-string 1 str)))
+          (emacs-surround-wrap match t-prefix t-suffix)))))
+
+(defun emacs-surround-cut-region ()
+  "Cut region at point."
+  (buffer-substring (region-beginning) (region-end)))
+
+(defun emacs-surround-helper (mark-fn prefix suffix)
+  "Helper emacs-suround (inset|delte|line|change).
+MARK-FN PREFIX SUFFIX."
+  (unless (use-region-p) (funcall mark-fn))
+  (let* ((target-str (emacs-surround-cut-region))
+         (replaced-str (emacs-surround-replace
+                        target-str
+                        (emacs-surround-get-alist prefix)
+                        (emacs-surround-get-alist suffix))))
+    (if replaced-str
+        (progn
+          (delete-region (region-beginning) (region-end))
+          (insert replaced-str))
+      (message "not found prefix and suffix"))))
+
+(defun emacs-surround-insert (str)
+  "Insert surround word STR."
+  (emacs-surround-helper 'emacs-surround-mark-region-sep "" str))
+
+(defun emacs-surround-delete (str)
+  "Delete surround word which is STR."
+  (let ((s (emacs-surround-get-alist str)))
+    (emacs-surround-helper (lambda () (emacs-surround-mark-between (car s) (cdr s)))
+                           str "")))
+
+(defun emacs-surround-line (str)
+  "Wrap line with STR."
+  (emacs-surround-helper 'emacs-surround-mark-region-line "" str))
+
+(defun emacs-surround-change (to end)
+  "Change suround string TO into END."
+  (let ((s (emacs-surround-get-alist to)))
+    (emacs-surround-helper (lambda () (emacs-surround-mark-between (car s) (cdr s)))
+                           to end)))
+
+;;;###autoload
+(defun emacs-surround ()
+  "Surround or Delete symbol etc.
+if cmd1 is i(insert), surround at-point-symbol.
+if cmd1 is d(delete), delete surround cmd2 char.
+if cmd1 is l(line), surround line which point is here.
+else change surround cmd1 to cmd2"
+  (interactive)
+  (let* ((cmd1 (char-to-string (read-char)))
+         (_cmd2 (read-char))
+         (cmd2 (char-to-string _cmd2)))
+    (cond ((string= cmd1 "i") (emacs-surround-insert cmd2))
+          ((string= cmd1 "d") (emacs-surround-delete cmd2))
+          ((string= cmd1 "l") (emacs-surround-line cmd2))
+          (t (if (= 13 _cmd2)              ;return
+                 (emacs-surround-insert cmd1)
+               (emacs-surround-change cmd1 cmd2))))))
+
+(provide 'emacs-surround)
 
 ;;; emacs-surround.el ends here
